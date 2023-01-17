b3df00a92a9a5d86e20ba73a90aba250cc681810
SarutThe1 <sarut.the@gmail.com>
2023-01-17 16:13:40 +0700

    first commit

---
+++ frontend/src/components/ProductList.jsx
@@ -0,0 +1,112 @@
+import React, {eseEffect,useState} from 'react'
+import {Link} from 'react-router-dom'
+import {Button} from 'react-bootstrap'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+import { useEffect } from 'react'
+
+
+function ProductList() {
+
+    const [products,setProducts] = useState([]);
+
+    useEffect(()=>{
+        fetchProducts();
+    },[]);
+
+    const fetchProducts = async () => {
+        await axios.get(`http://localhost:8000/api/products`)
+        .then(({data}) => {
+            setProducts(data);
+        })
+    }
+
+    const deleteProduct = async (id) => {
+        const isConfirm = await Swal.fire({
+            title: 'Are you sure to delete?',
+            text: "You won't be able to undo",
+            icon: 'warning',
+            showCancelButton:true,
+            confirmButtonColor:'red',
+            cancelButtonColor:'gray',
+            confirmButtonText:'Confirm Delete'
+        }).then((result)=>{
+            return result.isConfirmed;
+        })
+
+        if(!isConfirm) {
+            return;
+        }
+
+        await axios.delete(`http://localhost:8000/api/products/${id}`).then(({data})=> {
+            Swal.fire({
+                icon:'success',
+                text: data.message
+            })
+            fetchProducts()
+        }).catch(({response:{data}}) => {
+            Swal.fire({
+                icon:'error',
+                text: data.message
+            })
+        })
+    }
+
+
+
+  return (
+    <div className='container'>
+        <div className='row'>
+            <div className="col-12">
+                <Link className='btn btn-primary mb-2 float-end' to={'/product/create'} >
+                    Create Product
+                </Link>
+            </div>
+            <div className="col-12">
+                <div className="card card-body">
+                    <div className="table-responsive">
+                        <table className="table table-bordered mb-0 text-center">
+                            <thead>
+                                <tr>
+                                    <td>Title</td>
+                                    <td>Description</td>
+                                    <td>Image</td>
+                                    <td>Actions</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {products.length > 0 ? (
+                                    products.map((row,key) => (
+                                        <tr key={key}>
+                                            <td>{row.title}</td>
+                                            <td>{row.description}</td>
+                                            <td>
+                                                <img width='50px' src={`http://localhost:8000/storage/product/image/${row.image}`} alt="" />
+                                            </td>
+                                            <td>
+                                                <Link to={`/product/edit/${row.id}`} className='btn btn-success me-2' >
+                                                    Edit
+                                                </Link>
+                                                <Button variant='danger' onClick={()=> deleteProduct(row.id)}>
+                                                    Delete
+                                                </Button>
+
+                                            </td>
+                                        </tr>
+                                    ))
+                                ) : (
+                                    <tr>
+                                        <td colSpan='4'>No product Found</td>
+                                    </tr>
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductList
